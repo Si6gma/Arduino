cc9e245bed6e118d29c6ee5e2d24e3b895af3be0
Neiv Malaviya <90695450+Si6gma@users.noreply.github.com>
2023-07-08 11:06:48 +0530

    Started ESP Project

---
+++ ESP/src/blink.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+int ledPin = 2;
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  Serial.println("High");
+  digitalWrite(ledPin, HIGH);
+
+  delay(500);
+  Serial.println("Low");
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}
